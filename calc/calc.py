96398e6a9f7cf9459ba902ec6b2994be092d4424
muathendirangu <muathe.ndirangu@gmail.com>
2021-06-23 21:42:45 +0300

    add calculator test cases

--- calc/calc.py
+++ calc/calc.py
@@ -1,1 +1,31 @@
 """Main module."""
+from functools import reduce
+class Calc:
+    def add(self,*args):
+        return sum(args)
+    
+    def sub(self,a,b):
+        return a - b
+    
+    def mul(self,*args):
+        if not all(args):
+            raise ValueError
+        return reduce(lambda a,b:a*b,args)
+    
+    def div(self, a, b):
+        if not b:
+            return "infinite"
+        return a / b
+    
+    def avg(self, it, ut=None, lt=None):
+        if not len(it):
+            return 0
+        if not ut:
+            ut = max(it)
+        if not lt:
+            lt = min(it)
+        _it = [x for x in it if x >= lt and x <= ut]
+        if not _it:
+            return 0
+        return sum(_it)/len(_it)
+    
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -5,20 +5,98 @@
 import pytest
 
 
-from calc import calc
+from calc.calc import Calc
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_add_two_numbers():
+    c  = Calc()
+    res = c.add(4,5)
+    assert res == 9
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_add_three_numbers():
+    c= Calc()
+    res = c.add(12,1,6)
+    assert res == 19
+
+
+
+def test_add_many_numbers():
+    n= range(100)
+
+    res = Calc.add(*n)
+    assert res == 4950
+
+def test_subtract_two_numbers():
+    c = Calc()
+    res = c.sub(10, 3)
+    assert res == 7
+
+def test_mul_two_numbers():
+    c = Calc()
+    res = c.mul(10, 300)
+    assert res == 3000
+
+
+def test_mul_multiple_numbers():
+    numbers = range(1,10)
+    c=Calc()
+    res = c.mul(*numbers)
+    assert res == 362880
+
+
+def test_div_two_numbers_float():
+    c = Calc()
+    res = c.div(13, 2)
+    assert res == 6.5
+
+def test_div_by_zero_returns_infinite():
+    c = Calc()
+    res = c.div(5, 0)
+    assert res == "infinite"
+
+def test_mul_by_zero_raises_exception():
+    c = Calc()
+    with pytest.raises(ValueError):
+        c.mul(3, 0)
+
+def test_avg_correct_average():
+    c = Calc()
+    res = c.avg([2, 5, 12, 98])
+    assert res == 29.25
+
+def test_avg_removes_upper_outliers():
+    c = Calc()
+    res = c.avg([2, 5, 12, 98], ut=90)
+    assert res == pytest.approx(6.333333)
+
+def test_avg_removes_lower_outliers():
+    c = Calc()
+    res = c.avg([2, 5, 12, 98], lt=10)
+    assert res == pytest.approx(55)
+
+def test_avg_uppper_threshold_is_included():
+    c = Calc()
+    res = c.avg([2, 5, 12, 98], ut=98)
+    assert res == 29.25
+
+def test_avg_lower_threshold_is_included():
+    c = Calc()
+    res = c.avg([2, 5, 12, 98], lt=2)
+    assert res == 29.25
+
+def test_avg_empty_list():
+    c = Calc()
+    res = c.avg([])
+    assert res == 0
+
+def test_avg_manages_empty_list_after_outlier_removal():
+    c = Calc()
+    res = c.avg([12, 98], lt=15, ut=90)
+    assert res == 0
+
+def test_avg_manages_empty_list_before_outlier_removal():
+    c = Calc()
+    res = c.avg([], lt=15, ut=90)
+    assert res == 0
